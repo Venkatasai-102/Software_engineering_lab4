694f37d3e3a7ff1921ffc306451e53ab4563d1b1
Abir Das <abir.das@email.ucr.edu>
2022-02-09 13:08:34 +0530

    Tkinter assignment

---
+++ Python_Tkinter_Assignment/ImageViewerGUI.py
@@ -0,0 +1,79 @@
+####### REQUIRED IMPORTS FROM THE PREVIOUS ASSIGNMENT #######
+from my_package.model import InstanceSegmentationModel
+from my_package.data import Dataset
+from my_package.analysis import plot_visualization
+from my_package.data.transforms import FlipImage, RescaleImage, BlurImage, CropImage, RotateImage
+from PIL import Image
+
+####### ADD THE ADDITIONAL IMPORTS FOR THIS ASSIGNMENT HERE #######
+
+
+# Define the function you want to call when the filebrowser button is clicked.
+def fileClick(clicked, dataset, segmentor):
+
+	####### CODE REQUIRED (START) #######
+	# This function should pop-up a dialog for the user to select an input image file.
+	# Once the image is selected by the user, it should automatically get the corresponding outputs from the segmentor.
+	# Hint: Call the segmentor from here, then compute the output images from using the `plot_visualization` function and save it as an image.
+	# Once the output is computed it should be shown automatically based on choice the dropdown button is at.
+	# To have a better clarity, please check out the sample video.
+
+	####### CODE REQUIRED (END) #######
+
+# `process` function definition starts from here.
+# will process the output when clicked.
+def process(clicked):
+
+	####### CODE REQUIRED (START) #######
+	# Should show the corresponding segmentation or bounding boxes over the input image wrt the choice provided.
+	# Note: this function will just show the output, which should have been already computed in the `fileClick` function above.
+	# Note: also you should handle the case if the user clicks on the `Process` button without selecting any image file.
+
+	####### CODE REQUIRED (END) #######
+
+# `main` function definition starts from here.
+if __name__ == '__main__':
+
+	####### CODE REQUIRED (START) ####### (2 lines)
+	# Instantiate the root window.
+	# Provide a title to the root window.
+	
+	####### CODE REQUIRED (END) #######
+
+	# Setting up the segmentor model.
+	annotation_file = './data/annotations.jsonl'
+	transforms = []
+
+	# Instantiate the segmentor model.
+	segmentor = InstanceSegmentationModel()
+	# Instantiate the dataset.
+	dataset = Dataset(annotation_file, transforms=transforms)
+
+	
+	# Declare the options.
+	options = ["Segmentation", "Bounding-box"]
+	clicked = StringVar()
+	clicked.set(options[0])
+
+	e = Entry(root, width=70)
+	e.grid(row=0, column=0)
+
+	####### CODE REQUIRED (START) #######
+	# Declare the file browsing button
+
+	####### CODE REQUIRED (END) #######
+
+	####### CODE REQUIRED (START) #######
+	# Declare the drop-down button
+
+	####### CODE REQUIRED (END) #######
+
+	# This is a `Process` button, check out the sample video to know about its functionality
+	myButton = Button(root, text="Process", command=partial(process, clicked))
+	myButton.grid(row=0, column=3)
+
+	
+	####### CODE REQUIRED (START) ####### (1 line)
+	# Execute with mainloop()
+
+	####### CODE REQUIRED (END) #######
